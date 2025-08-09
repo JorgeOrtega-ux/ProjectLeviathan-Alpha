8f69bf73bde32011a14297b64039407dd482d196
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-08-09 12:42:38 -0600

    Add files via upload

--- ProjectLeviathan/includes/modules/module-options.php
+++ ProjectLeviathan/includes/modules/module-options.php
@@ -3,37 +3,39 @@
         <div class="pill-container">
             <div class="drag-handle"></div>
         </div>
-        <div class="menu-list overflow-y">
-            <div class="menu-link">
-                <div class="menu-link-icon">
-                    <span class="material-symbols-rounded">settings</span>
+        <div class="menu-body overflow-y">
+            <div class="menu-list">
+                <div class="menu-link">
+                    <div class="menu-link-icon">
+                        <span class="material-symbols-rounded">settings</span>
+                    </div>
+                    <div class="menu-link-text">
+                        <span>Configuracion</span>
+                    </div>
                 </div>
-                <div class="menu-link-text">
-                    <span>Configuracion</span>
+                <div class="menu-link">
+                    <div class="menu-link-icon">
+                        <span class="material-symbols-rounded">help</span>
+                    </div>
+                    <div class="menu-link-text">
+                        <span>Ayuda y recursos</span>
+                    </div>
                 </div>
-            </div>
-            <div class="menu-link">
-                <div class="menu-link-icon">
-                    <span class="material-symbols-rounded">help</span>
+                <div class="menu-link">
+                    <div class="menu-link-icon">
+                        <span class="material-symbols-rounded">shopping_cart</span>
+                    </div>
+                    <div class="menu-link-text">
+                        <span>Historial de compras</span>
+                    </div>
                 </div>
-                <div class="menu-link-text">
-                    <span>Ayuda y recursos</span>
-                </div>
-            </div>
-            <div class="menu-link">
-                <div class="menu-link-icon">
-                    <span class="material-symbols-rounded">shopping_cart</span>
-                </div>
-                <div class="menu-link-text">
-                    <span>Historial de compras</span>
-                </div>
-            </div>
-            <div class="menu-link">
-                <div class="menu-link-icon">
-                    <span class="material-symbols-rounded">exit_to_app</span>
-                </div>
-                <div class="menu-link-text">
-                    <span>Cerrar sesion</span>
+                <div class="menu-link">
+                    <div class="menu-link-icon">
+                        <span class="material-symbols-rounded">exit_to_app</span>
+                    </div>
+                    <div class="menu-link-text">
+                        <span>Cerrar sesion</span>
+                    </div>
                 </div>
             </div>
         </div>
