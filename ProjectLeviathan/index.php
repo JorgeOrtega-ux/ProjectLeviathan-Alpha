e652acd43b71f092493e1819402f158c49bbac3e
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-08-08 23:34:15 -0600

    Add files via upload

---
+++ ProjectLeviathan/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="page-wrapper">
+        <div class="main-content">
+            <div class="general-content">
+                <div class="general-content-top">
+                    <div class="header">
+                        <div class="header-left"></div>
+                        <div class="header-right">
+                            <div class="header-item"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="general-content-bottom"></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
