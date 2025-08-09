e85446e68a35369c34cc4c81d32c2c1bb19a3665
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-08-09 00:13:41 -0600

    Add files via upload

--- ProjectLeviathan/index.php
+++ ProjectLeviathan/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
     <title>Document</title>
 </head>
 
@@ -16,14 +17,54 @@
                     <div class="header">
                         <div class="header-left"></div>
                         <div class="header-right">
-    <div class="header-item">
-        <div class="profile-container">
-            <div class="profile-content">
-                J
-            </div>
-        </div>
-        </div>
-</div>
+                            <!-- Owner -->
+                            <div class="header-item">
+                                <div class="profile-container rank-owner" data-action="toggleModuleOptions">
+                                    <div class="profile-content">
+                                        <span class="material-symbols-rounded">shield</span>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="module-content module-options body-title disabled" data-module="moduleOptions">
+                            <div class="menu-content">
+                                <div class="menu-list">
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">settings</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Configuracion</span>
+                                        </div>
+                                    </div>
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">help</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Ayuda y recursos</span>
+                                        </div>
+                                    </div>
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">shopping_cart</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Historial de recursos</span>
+                                        </div>
+                                    </div>
+                                    <div class="menu-link">
+                                        <div class="menu-link-icon">
+                                            <span class="material-symbols-rounded">exit_to_app</span>
+                                        </div>
+                                        <div class="menu-link-text">
+                                            <span>Cerrar sesion</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="general-content-bottom"></div>
