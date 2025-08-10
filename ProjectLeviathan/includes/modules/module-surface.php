5f3c9aded887ea921ee28b253d8caf00b59c878f
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-08-09 21:06:46 -0600

    Add files via upload

---
+++ ProjectLeviathan/includes/modules/module-surface.php
@@ -0,0 +1,22 @@
+<div class="module-content module-surface body-title disabled" data-module="moduleSurface">
+    <div class="menu-content">
+        <div class="menu-list">
+            <div class="menu-link active">
+                <div class="menu-link-icon">
+                    <span class="material-symbols-rounded">home</span>
+                </div>
+                <div class="menu-link-text">
+                    <span>Pagina principal</span>
+                </div>
+            </div>
+            <div class="menu-link">
+                <div class="menu-link-icon">
+                    <span class="material-symbols-rounded">settings</span>
+                </div>
+                <div class="menu-link-text">
+                    <span>Explorar comunidades</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- ProjectLeviathan/index.php
+++ ProjectLeviathan/index.php
@@ -18,28 +18,7 @@
                 </div>
                 <div class="general-content-bottom">
                     <div class="general-content-scrolleable">
-                        <div class="module-content module-surface body-title disabled" data-module="moduleSurface">
-                            <div class="menu-content">
-                                <div class="menu-list">
-                                    <div class="menu-link active">
-                                        <div class="menu-link-icon">
-                                            <span class="material-symbols-rounded">home</span>
-                                        </div>
-                                        <div class="menu-link-text">
-                                            <span>Pagina principal</span>
-                                        </div>
-                                    </div>
-                                    <div class="menu-link">
-                                        <div class="menu-link-icon">
-                                            <span class="material-symbols-rounded">settings</span>
-                                        </div>
-                                        <div class="menu-link-text">
-                                            <span>Explorar comunidades</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <?php include 'includes/modules/module-surface.php'; ?>
                     </div>
                 </div>
             </div>
