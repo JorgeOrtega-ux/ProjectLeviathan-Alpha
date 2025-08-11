9347f5c1117147ca2e36f988cbd627d2723740e0
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-08-11 12:35:58 -0600

    Add files via upload

---
+++ ProjectLeviathan/includes/sections/settings/profile-section.php
@@ -0,0 +1,3 @@
+<div class="section-content <?php echo $CURRENT_SUBSECTION === 'profile' ? 'active' : 'disabled'; ?>" data-section="sectionProfile">
+    <div class="settings-container"></div>
+</div>
