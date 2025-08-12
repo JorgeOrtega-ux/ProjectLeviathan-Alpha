2820506aa9b1bf39006d5ed065eb991e0bc1942c
JorgeOrtega-ux <jorge1ortega.484@gmail.com>
2025-08-12 02:34:30 -0500

    Delete ProjectLeviathan directory

--- ProjectLeviathan/includes/sections/settings/profile-section.php
+++
@@ -1,3 +0,0 @@
-<div class="section-content <?php echo $CURRENT_SUBSECTION === 'profile' ? 'active' : 'disabled'; ?>" data-section="sectionProfile">
-    <div class="settings-container"></div>
-</div>
